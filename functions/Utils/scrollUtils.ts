4c6ee9de214a5f6498aa0edcb82fa0867d630f11
Santiago <canepasantiago.ivan@gmail.com>
2024-07-10 15:21:40 -0300

    u

--- functions/Utils/scrollUtils.ts
+++ functions/Utils/scrollUtils.ts
@@ -1,22 +1,37 @@
 import { Page } from 'puppeteer'
 
+
 export async function scrollModal(page: Page, outerModalSelector: string, innerModalSelector: string): Promise<boolean> {
   const scrolled = await page.evaluate((outerModalSelector, innerModalSelector) => {
     const outerModal = document.querySelector(outerModalSelector);
     const innerModal = outerModal?.querySelector(innerModalSelector);
 
     if (innerModal) {
-      const randomScroll = Math.random();
-      if (randomScroll < 0.2) {
-        // 20% de probabilidad de scroll lento
-        innerModal.scrollBy(0, innerModal.clientHeight * 0.8);
-      } else if (randomScroll < 0.5) {
-        // 50% de probabilidad de scroll medio
-        innerModal.scrollBy(0, innerModal.clientHeight * 1.4);
-      } else {
-        // 30% de probabilidad de scroll rápido
-        innerModal.scrollBy(0, innerModal.clientHeight * 3);
+      const randomScroll = () => {
+        const rand = Math.random();
+        if (rand < 0.2) {
+          // 20% de probabilidad de scroll lento
+          innerModal.scrollBy(0, innerModal.clientHeight * 0.8);
+        } else if (rand < 0.4) {
+          // 20% de probabilidad de scroll hacia arriba
+          innerModal.scrollBy(0, -innerModal.clientHeight * 1.8);
+        } else if (rand < 0.6) {
+          // 20% de probabilidad de scroll medio hacia abajo
+          innerModal.scrollBy(0, innerModal.clientHeight * 1.4);
+        } else if (rand < 0.8) {
+          // 20% de probabilidad de scroll largo hacia arriba
+          innerModal.scrollBy(0, -innerModal.clientHeight * 3.5);
+        } else {
+          // 20% de probabilidad de scroll rápido
+          innerModal.scrollBy(0, innerModal.clientHeight * 3);
+        }
+      };
+
+      // Realizar múltiples scrolleos
+      for (let i = 0; i < 3; i++) {
+        randomScroll();
       }
+
       return true;
     }
     return false;
